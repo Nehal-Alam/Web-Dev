e35ef5bd59b1f38a9fb2745467507ca033813191
NEHAL ALAM <109755340+Nehal-Alam@users.noreply.github.com>
2023-12-11 21:54:51 +0530

    Discord clone

---
+++ Tailwind CSS/Discord/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+  scale: {
+        '135': '1.35',
+      }
+}
