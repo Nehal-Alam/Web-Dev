cfdb6321743c578c77edb2290ddee1fbb5693b5e
Nehal Alam <1309alamnehal@gmail.com>
2024-02-22 20:33:31 +0530

    String

---
+++ JavaScript/JavaScript Day - 3/string.js
@@ -0,0 +1,19 @@
+let str = "This is a string.";
+let str1 = "Hello  World!";
+
+console.log(str.toUpperCase()); // Outputs: THIS IS A STRING.
+console.log(str.toLowerCase()); // Outputs: this is a string.
+console.log(str.length); // Outputs: 17
+console.log(str.charAt(3));
+console.log(str.at(2));
+console.log(str.concat(str1));
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd());
+console.log(str.padStart());
+console.log(str.padEnd());
+console.log(str.codePointAt(2));
+console.log(str.replace("This is a string", "Hello World"));
+console.log(str.replaceAll("a", "A"));
+console.log(str.split()); 
+
